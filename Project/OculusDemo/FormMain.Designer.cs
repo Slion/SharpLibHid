8df0b27219a145d307a7f3839c9563759fd6fbe1
Stéphane Lenclud <github@lenclud.com>
2021-07-04 00:13:31 +0200

    Move Oculus data structures and parsing code its own file.

--- Project/OculusDemo/FormMain.Designer.cs
+++ Project/OculusDemo/FormMain.Designer.cs
@@ -32,7 +32,7 @@ namespace OculusDemo
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "FormMain";
         }
 
         #endregion
