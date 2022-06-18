cf04f8b96db652a86b7d959dcb68e61e1efc36ae
Stéphane Lenclud <github@lenclud.com>
2022-06-18 12:16:30 +0200

    Start work on fetching device properties

--- Project/Hid/CsWin32.cs
+++ Project/Hid/CsWin32.cs
@@ -21,7 +21,50 @@ namespace Windows.Win32
     }
 
 
-
+    namespace Devices.Properties
+    {
+        [Flags]
+        public enum DEVPROP_TYPE_FLAGS : uint
+        {
+            DEVPROP_TYPEMOD_ARRAY = 0x00001000,  // array of fixed-sized data elements
+            DEVPROP_TYPEMOD_LIST = 0x00002000,  // list of variable-sized data elements
+            // Property data types.
+            DEVPROP_TYPE_EMPTY = 0x00000000,  // nothing, no property data
+            DEVPROP_TYPE_NULL = 0x00000001,  // null property data
+            DEVPROP_TYPE_SBYTE = 0x00000002,  // 8-bit signed int (SBYTE)
+            DEVPROP_TYPE_BYTE = 0x00000003,  // 8-bit unsigned int (BYTE)
+            DEVPROP_TYPE_INT16 = 0x00000004,  // 16-bit signed int (SHORT)
+            DEVPROP_TYPE_UINT16 = 0x00000005,  // 16-bit unsigned int (USHORT)
+            DEVPROP_TYPE_INT32 = 0x00000006,  // 32-bit signed int (LONG)
+            DEVPROP_TYPE_UINT32 = 0x00000007,  // 32-bit unsigned int (ULONG)
+            DEVPROP_TYPE_INT64 = 0x00000008,  // 64-bit signed int (LONG64)
+            DEVPROP_TYPE_UINT64 = 0x00000009,  // 64-bit unsigned int (ULONG64)
+            DEVPROP_TYPE_FLOAT = 0x0000000A,  // 32-bit floating-point (FLOAT)
+            DEVPROP_TYPE_DOUBLE = 0x0000000B,  // 64-bit floating-point (DOUBLE)
+            DEVPROP_TYPE_DECIMAL = 0x0000000C,  // 128-bit data (DECIMAL)
+            DEVPROP_TYPE_GUID = 0x0000000D,  // 128-bit unique identifier (GUID)
+            DEVPROP_TYPE_CURRENCY = 0x0000000E,  // 64 bit signed int currency value (CURRENCY)
+            DEVPROP_TYPE_DATE = 0x0000000F,  // date (DATE)
+            DEVPROP_TYPE_FILETIME = 0x00000010,  // file time (FILETIME)
+            DEVPROP_TYPE_BOOLEAN = 0x00000011,  // 8-bit boolean (DEVPROP_BOOLEAN)
+            DEVPROP_TYPE_STRING = 0x00000012,  // null-terminated string
+            DEVPROP_TYPE_STRING_LIST = (DEVPROP_TYPE_STRING|DEVPROP_TYPEMOD_LIST), // multi-sz string list
+            DEVPROP_TYPE_SECURITY_DESCRIPTOR = 0x00000013,  // self-relative binary SECURITY_DESCRIPTOR
+            DEVPROP_TYPE_SECURITY_DESCRIPTOR_STRING = 0x00000014,  // security descriptor string (SDDL format)
+            DEVPROP_TYPE_DEVPROPKEY = 0x00000015,  // device property key (DEVPROPKEY)
+            DEVPROP_TYPE_DEVPROPTYPE = 0x00000016,  // device property type (DEVPROPTYPE)
+            DEVPROP_TYPE_BINARY = (DEVPROP_TYPE_BYTE |DEVPROP_TYPEMOD_ARRAY),  // custom binary data
+            DEVPROP_TYPE_ERROR = 0x00000017,  // 32-bit Win32 system error code
+            DEVPROP_TYPE_NTSTATUS = 0x00000018,  // 32-bit NTSTATUS code
+            DEVPROP_TYPE_STRING_INDIRECT = 0x00000019,  // string resource (@[path\]<dllname>,-<strId>)
+            // Max base DEVPROP_TYPE_ and DEVPROP_TYPEMOD_ values.
+            MAX_DEVPROP_TYPE = 0x00000019,  // max valid DEVPROP_TYPE_ value
+            MAX_DEVPROP_TYPEMOD = 0x00002000,  // max valid DEVPROP_TYPEMOD_ value
+            // Bitmasks for extracting DEVPROP_TYPE_ and DEVPROP_TYPEMOD_ values.
+            DEVPROP_MASK_TYPE = 0x00000FFF,  // range for base DEVPROP_TYPE_ values
+            DEVPROP_MASK_TYPEMOD = 0x0000F000  // mask for DEVPROP_TYPEMOD_ type modifiers
+        }
+    }
 
     namespace Devices.DeviceAndDriverInstallation
     {
--- Project/Hid/Setup/Device.cs
+++ Project/Hid/Setup/Device.cs
@@ -12,6 +12,7 @@ namespace SharpLib.Setup
     using Windows.Win32;
     using Windows.Win32.Foundation;
     using Windows.Win32.Devices.DeviceAndDriverInstallation;
+    using Windows.Win32.Devices.Properties;
     using SharpLib.Win32;
     using System.Diagnostics;
     using System.ComponentModel;
@@ -25,10 +26,10 @@ namespace SharpLib.Setup
     /// </summary>
     public class Device : IDisposable
     {
-
+        private string iInstancePath;
         SetupDiDestroyDeviceInfoListSafeHandle iDevInfo;
         SP_DEVINFO_DATA iDevInfoData = new SP_DEVINFO_DATA(true);
-        string iInstancePath;
+        
 
         /// <summary>
         /// Instance path uniquely identifies a device.
@@ -39,39 +40,82 @@ namespace SharpLib.Setup
             get { return iInstancePath; }
             protected set
             {
-                unsafe
-                {
-                    iInstancePath = value;
-                    // Get our device information set handle, that set though should contain only this device instance really
-                    iDevInfo = PInvoke.SetupDiGetClassDevs(null, iInstancePath, new HWND(0), (uint)(DEVICEINFOGETCLASS_FLAGS.DIGCF_ALLCLASSES | DEVICEINFOGETCLASS_FLAGS.DIGCF_DEVICEINTERFACE));
-                    if (iDevInfo.IsInvalid)
-                    {
-                        //GetLastError.Log();
-                        //return;
-                        GetLastError.LogAndThrow("SetupDiGetClassDevs");
-                    }
-
-                    uint index = 0;
-                    // Get our device information data
-                    fixed (SP_DEVINFO_DATA* ptrDevInfoData = &iDevInfoData) // Needed for data members apparently 
-                    {                        
-                        if (!PInvoke.SetupDiEnumDeviceInfo(iDevInfo, index++, ptrDevInfoData))
-                        {
-                            GetLastError.LogAndThrow("SetupDiEnumDeviceInfo");
-                        }
-                    }
-
-                    // Make sure we only have one item in our list
-                    SP_DEVINFO_DATA discard = new SP_DEVINFO_DATA(true);
-                    if (PInvoke.SetupDiEnumDeviceInfo(iDevInfo, index++, &discard))
-                    {
-                        throw new ArgumentException("More than one device in our information set");
-                        //GetLastError.LogAndThrow("SetupDiEnumDeviceInfo");
-                    }
-                }
+                iInstancePath = value;
+                GetDeviceInfoData();
+                GetAllProperties();
+   
             } 
         }
 
+        private unsafe void GetDeviceInfoData()
+        {
+            // Get our device information set handle, that set though should contain only this device instance really
+            iDevInfo = PInvoke.SetupDiGetClassDevs(null, InstancePath, new HWND(0), (uint)(DEVICEINFOGETCLASS_FLAGS.DIGCF_ALLCLASSES | DEVICEINFOGETCLASS_FLAGS.DIGCF_DEVICEINTERFACE));
+            if (iDevInfo.IsInvalid)
+            {
+                //GetLastError.Log();
+                //return;
+                GetLastError.LogAndThrow("SetupDiGetClassDevs");
+            }
+
+            uint index = 0;
+            // Get our device information data
+            fixed (SP_DEVINFO_DATA* ptrDevInfoData = &iDevInfoData) // Needed for data members apparently 
+            {
+                if (!PInvoke.SetupDiEnumDeviceInfo(iDevInfo, index++, ptrDevInfoData))
+                {
+                    GetLastError.LogAndThrow("SetupDiEnumDeviceInfo");
+                }
+            }
+
+            // Make sure we only have one item in our list
+            SP_DEVINFO_DATA discard = new SP_DEVINFO_DATA(true);
+            if (PInvoke.SetupDiEnumDeviceInfo(iDevInfo, index++, &discard))
+            {
+                throw new ArgumentException("More than one device in our information set");
+                //GetLastError.LogAndThrow("SetupDiEnumDeviceInfo");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        unsafe void GetAllProperties()
+        {
+            fixed (SP_DEVINFO_DATA* ptrDevInfoData = &iDevInfoData) // Needed for data members apparently 
+            {
+                uint propertyCount;
+                // First get our number of properties
+                PInvoke.SetupDiGetDevicePropertyKeys(iDevInfo, ptrDevInfoData, null, 0, &propertyCount, 0); // Returns false and ERROR_NO_TOKEN when querying size
+                if (propertyCount<=0)
+                {
+                    return;
+                }
+
+                DEVPROPKEY* propertyKeys = stackalloc DEVPROPKEY[(int)propertyCount];
+                if (!PInvoke.SetupDiGetDevicePropertyKeys(iDevInfo, ptrDevInfoData, propertyKeys, propertyCount, null, 0))
+                {
+                    GetLastError.LogAndThrow("SetupDiGetDevicePropertyKeys");
+                }
+
+                // Fetch all our properties
+                for (int i=0;i<propertyCount;i++)
+                {
+                    // Query our property type and size
+                    DEVPROP_TYPE_FLAGS type;
+                    uint size = 0;
+                    PInvoke.SetupDiGetDeviceProperty(iDevInfo, ptrDevInfoData, &propertyKeys[i], (uint*)&type, null, 0, &size, 0); // Returns false and ERROR_NO_TOKEN when querying size and type
+                    
+                    // TODO: Handle property types
+                    //if (PInvoke.SetupDiGetDeviceProperty(iDevInfo, ptrDevInfoData, &propertyKeys[i], (uint*)&type, null, 0, &size, 0))
+                    //{
+                    //    GetLastError.LogAndThrow("SetupDiGetDeviceProperty");
+                    //}
+                }
+
+            }
+
+        }
 
         /// <summary>
         /// Make sure dispose is called even if the user forgot about it.
